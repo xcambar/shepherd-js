94a54ed9afe2bd0971d5676ffdda303e86973972
Xavier Cambar <xavier@gmail.com>
2012-01-19 16:20:22 +0100

    Moved the parsing function out of the request callback

--- loader.js
+++ loader.js
@@ -1,16 +1,51 @@
 /**
  * !! Module declaration MUST be the header of the file
  * 
- * See http://wiki.ecmascript.org/doku.php?id=harmony:modules for parsing and use case reference 
+ * @see http://wiki.ecmascript.org/doku.php?id=harmony:modules for parsing and use case reference
+ * @see http://addyosmani.com/writing-modular-js/
  */
 (function (me) {
-    
-    
-    
-    
     var modules = {},
         _errCb,
         _errModules = null;
+    
+    /**
+     * Parses the module declaration and prepares the execution context of the module
+     * @param {Array} An array of text of lines. Each line is a fragment of a module declaration
+     * @return {Object|String} Returns the module's evaluated execution context or an error string 
+     */
+    var parse = function (declaration) {
+        var moduleConf = {};
+    	for (var j = 0; j < declaration.length; j++) {
+    		var cmd = declaration[j];
+            var tokens = cmd.pop().split(' '),
+                fn = tokens.shift();
+            switch (fn) {
+                case 'module':
+                    if (tokens[0] && tokens[0] !== 'from') {
+                        moduleConf.name = tokens[0];
+                    }
+                    break;
+                case 'export':
+                    moduleConf.export = moduleConf.export || [];
+                    moduleConf.export.push(tokens[0]);
+                    break;
+                case 'import':
+                    moduleConf.import = moduleConf.import || {};
+                    tokens[0].split(/\s*,\s*/).forEach(function (v) {
+                        moduleConf.import[v] = modules[v];
+                    });
+                    break;
+                case '}': //@TODO Closes the module definition
+                    break;
+                default:
+                    return 'Unknown token: ' + fn;
+                    break;
+            }
+    	}
+    	return moduleConf;
+    };
+    
     /**
      * Retrieves the file corresponding to the module and declares it
      */
@@ -32,39 +67,20 @@
             },
             success: function (res) {
                 var text = res.responseText.split('\n');
-                var moduleConf = {};
+                var declaration = [],
+                	moduleConf = {};
                 for (var i = 0; i < text.length; i++) {
                     var cmd = text[i].trim().match(/^"\s*(.*)\s*\{?";?$/);
                     if (cmd) {
-                        var tokens = cmd.pop().split(' '),
-                            fn = tokens.shift();
-                        switch (fn) {
-                            case 'module':
-                                if (tokens[0] && tokens[0] !== 'from') {
-                                    moduleConf.name = tokens[0];
-                                }
-                                break;
-                            case 'export':
-                                moduleConf.export = moduleConf.export || [];
-                                moduleConf.export.push(tokens[0]);
-                                break;
-                            case 'import':
-                                moduleConf.import = moduleConf.import || {};
-                                tokens[0].split(/\s*,\s*/).forEach(function (v) {
-                                    moduleConf.import[v] = modules[v];
-                                });
-                                break;
-                            case '}': //@TODO Closes the module definition
-                                break;
-                            default:
-                                _error('Unknown token: ' + fn);
-                                break;
-                        }
+                    	moduleConf = declaration.push(cmd);
                     } else { // At the first line where we do not find a module-related command, we stop the module evaluation.
                         break;
                     }
                 }
-                
+                if (declaration.length) {
+                	moduleConf = parse(declaration);
+                	(typeof moduleConf == 'string') && _error(moduleConf); 
+                }
                 var returns = moduleConf.export ?
                     '{' + moduleConf.export.map(function (v) { return v + ':' + v; }).join(',') + '}'
                     : '';
