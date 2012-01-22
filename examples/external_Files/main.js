f84a9df501b4176d9481bbe75287cb1f77bcfa1a
Xavier Cambar <xcambar@gmail.com>
2012-01-22 22:30:26 +0100

    Added an example to demonstrate the use of plugins

---
+++ examples/external_Files/main.js
@@ -0,0 +1,8 @@
+"module {           ";
+"  module Underscore from _; ";
+"};                 ";
+
+var list = document.getElementsByTagName('ul')[0];
+for (var i in Underscore) {
+    Underscore.hasOwnProperty(i) && (list.innerHTML += ('<li>' + i + '</li>'));
+}
