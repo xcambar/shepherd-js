39a265a9df3ec14928e5d6fe29a00f8cb20dd809
Xavier Cambar <xcambar@gmail.com>
2012-02-10 09:05:58 +0100

    Started an example for NodeJS

---
+++ examples/NodeJS/app.js
@@ -0,0 +1,3 @@
+var s6d = require('../../shepherd.js');
+
+s6d('bootstrap.js');
---
+++ examples/NodeJS/bootstrap.js
@@ -0,0 +1,6 @@
+"module {                                       ";
+"  module fs from 'util';                       ";
+"  module _ from '../libs/underscore.min.js';   ";
+"}                                              ";
+
+console.log(_);
